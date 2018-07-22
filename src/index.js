3dff34ad5b18b8764df3012f20e0770ccb1ac36a
Adrian Acosta <aacosta@techlaunch>
2018-07-22 12:56:56 -0400

    optimized

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 const Info = {
     logo: "Big Bank, Inc",
     cardNumber: "1234 5678 8765 4321",
-    firstFour:"1234",
-    expiration:"8/19",
-    name:"CARDHOLDER NAME"
+    firstFour: "1234",
+    expiration: "8/19",
+    name: "CARDHOLDER NAME"
 };
 
-function Card() {
-    return (
+class Card extends Component {
+
+    render() {
+        return(
         <div className="card">
             <div className="logo">
-                <p className="logo-text">{this.props.logo}</p>
+                <p className="logo-text">{this.props.obj.logo}</p>
             </div>
-            {/* <div className="info">
-                <p className="card-number">{props.cardNumber}</p>
-                <p className="card-first-four">{props.firstFour}</p>
-                <p className="card-expiration">{props.expiration}</p>
-                <p className="card-name">{props.name}</p>
-            </div> */}
+            <div className="info">
+                <p className="card-number">{this.props.obj.cardNumber}</p>
+                <p className="card-first-four">{this.props.obj.firstFour}</p>
+                <p className="card-expiration">{this.props.obj.expiration}</p>
+                <p className="card-name">{this.props.obj.name}</p>
+            </div>
         </div>
-    );
+        )
+    }
+
 };
 
-ReactDOM.render(<Card logo="Big Bank , Inc"/>, document.getElementById('root'));
+
+ReactDOM.render(<Card obj={Info} />, document.getElementById('root'));
